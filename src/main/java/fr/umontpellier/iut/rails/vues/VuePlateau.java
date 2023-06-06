d2b71880e705b9800d2ba6ca221b078b2032676d
Petru Valicov <petru.valicov@umontpellier.fr>
2023-06-06 17:50:27 +0200

    correction noms villes

--- src/main/java/fr/umontpellier/iut/rails/vues/VuePlateau.java
+++ src/main/java/fr/umontpellier/iut/rails/vues/VuePlateau.java
@@ -49,6 +49,7 @@ public class VuePlateau extends Pane {
     };
 
     public void creerBindings() {
+ ajouterVilles();
         ajouterPorts();
         ajouterRoutes();
         bindRedimensionEtCentragePlateau();
@@ -177,4 +178,16 @@ public class VuePlateau extends Pane {
         });
     }
 
+
+
+private void ajouterVilles() {
+    for (String nomVille : DonneesGraphiques.villes.keySet()) {
+        DonneesGraphiques.DonneesCerclesPorts positionVilleSurPlateau = DonneesGraphiques.villes.get(nomVille);
+        Circle cercleVille = new Circle(positionVilleSurPlateau.centreX(), positionVilleSurPlateau.centreY(), DonneesGraphiques.rayonInitial);
+        cercleVille.setId(nomVille);
+        getChildren().add(cercleVille);
+        bindCerclePortAuPlateau(positionVilleSurPlateau, cercleVille);
+        cercleVille.setOnMouseClicked(choixPort);
+    }
+}
 }
