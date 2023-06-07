a68b23a10b6356ba3ddade0484118ae44c9ac899
Carriere Diego <diego.carriere@etu.umontpellier.fr>
2023-06-07 12:47:19 +0200

    VueResultats

--- src/main/java/fr/umontpellier/iut/rails/vues/VueResultats.java
+++ src/main/java/fr/umontpellier/iut/rails/vues/VueResultats.java
@@ -1,7 +1,20 @@
 package fr.umontpellier.iut.rails.vues;
 
+import fr.umontpellier.iut.rails.IJoueur;
 import fr.umontpellier.iut.rails.RailsIHM;
+import fr.umontpellier.iut.rails.mecanique.Joueur;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * Cette classe affiche les scores en fin de partie.
@@ -13,7 +26,30 @@ public class VueResultats extends Pane {
     private RailsIHM ihm;
 
     public VueResultats(RailsIHM ihm) {
-        this.ihm = ihm;
+
+        Stage stage = new Stage();
+
+        Label labelRes = new Label("Résultats du jeu");
+
+        GridPane gridPaneScore = new GridPane();
+        int i = 0;
+        for(Joueur j : ihm.getJeu().getJoueurs()){
+            gridPaneScore.add(new Label(j.getNom()),0,i);
+            Label score = new Label(j.getScore() + "");
+            gridPaneScore.add(score,1,i);
+            i++;
+        }
+
+        Button quitterBouton = new Button("Quitter");
+
+        quitterBouton.setOnAction(event -> {
+            Platform.exit();
+        });
+
+        VBox root = new VBox(labelRes,gridPaneScore,quitterBouton);
+
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 
 }
